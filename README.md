154cf7d0a270712aeccc0dc6d31987f77ccaf101
silviorsc <silvio.rogerio@gmail.com>
2021-05-05 22:24:15 -0300

    Fix PaymentPaidListener and add README.md

---
+++ README.md
@@ -0,0 +1,18 @@
+#spring-microservices-kafka
+
+Projeto para estudo da arquitetura em microserviços com duas aplicações integradas com Apache Kafka.
+
+O projeto do Bootcamp foi a criação de um Ecommerce dividido em 3 partes.
+
+    1 - microservico ecommerce-checkout-api 
+    2 - microservico ecommerce-payment-api
+    3 - ecommerce-checkout-frontend
+
+O projeto tem o seguinte setup:
+
+    -Java 15
+    -Spring Boot 2.4.4
+    -Spring Cloud Streams
+    -Docker 
+    -Zookeeper
+    -kafka
--- ecommerce-checkout-api-master/ecommerce-checkout-api-master/src/main/java/com/hatanaka/ecommerce/checkout/listener/PaymentPaidListener.java
+++ ecommerce-checkout-api-master/ecommerce-checkout-api-master/src/main/java/com/hatanaka/ecommerce/checkout/listener/PaymentPaidListener.java
@@ -4,7 +4,7 @@ import com.hatanaka.ecommerce.checkout.entity.CheckoutEntity;
 import com.hatanaka.ecommerce.checkout.repository.CheckoutRepository;
 import com.hatanaka.ecommerce.checkout.service.CheckoutService;
 import com.hatanaka.ecommerce.checkout.streaming.PaymentPaidSink;
-import com.hatanaka.ecommerce.payment.event.PaymentCreatedEvent;
+//import com.hatanaka.ecommerce.payment.event.PaymentCreatedEvent;
 import lombok.RequiredArgsConstructor;
 import org.springframework.cloud.stream.annotation.StreamListener;
 import org.springframework.stereotype.Component;
@@ -15,11 +15,11 @@ public class PaymentPaidListener {
 
     private final CheckoutRepository checkoutRepository;
 
-    @StreamListener(PaymentPaidSink.INPUT)
+   /* @StreamListener(PaymentPaidSink.INPUT)
     public void handler(PaymentCreatedEvent paymentCreatedEvent) {
         //checkoutService.updateStatus(paymentCreatedEvent.getCheckoutCode().toString(), CheckoutEntity.Status.APPROVED);
     final CheckoutEntity checkoutEntity = checkoutRepository.findByCode(paymentCreatedEvent.getCheckoutCode().toString()).orElseThrow();
    checkoutEntity.setStatus(CheckoutEntity.Status.APPROVED);
    checkoutRepository.save(checkoutEntity);
-    }
+    }*/
 }
